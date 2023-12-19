7a3e6c65b1f5a7d1023a07245b5daebccb0dc415
Bruno Membrado <brunomembrado10@gmail.com>
2023-12-19 11:09:09 -0300

    loginAWS

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifygraphqlawsrea72e6758d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
